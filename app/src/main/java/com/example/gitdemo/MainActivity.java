88eef6ab6e4a7ef4c2e331cede18c83bbc114b5c
zhongyuan.wu <zhongyuan.wu@thundersoft.com>
2022-08-25 18:07:38 +0800

    branch

--- app/src/main/java/com/example/gitdemo/MainActivity.java
+++ app/src/main/java/com/example/gitdemo/MainActivity.java
@@ -12,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toast.makeText(this, "Add Log", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "branch Version1.0", Toast.LENGTH_SHORT).show();
     }
 }
